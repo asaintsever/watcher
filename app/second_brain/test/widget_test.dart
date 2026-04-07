4fe94ce89684cc7e90f1fae68add9d4ecc779c45
Alain Saint-Sever <alstsever@free.fr>
2026-04-07 21:42:22 +0200

    Fixes

--- app/second_brain/test/widget_test.dart
+++ app/second_brain/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:watcher_mobile_app/main.dart';
+import 'package:second_brain/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
